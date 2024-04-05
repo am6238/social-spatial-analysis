59321585717201fec336afa02e5935bdb43a21e2
Alex Medoff <am6238@columbia.edu>
2024-04-05 11:16:53 -0400

    added classes.py and utilities.py

---
+++ python/classes.py
@@ -0,0 +1,18 @@
+#   04/05/2024 Alex Medoff
+#   -------------------------
+#   Define the classes to be used by this python project.
+#   ------------------------------------------------------
+
+# import libraries
+import numpy as np
+import os
+import re
+
+
+"""
+Define the main class: "dataset"
+
+TODO:
+
+"""
+
---
+++ python/utilities.py
@@ -0,0 +1,9 @@
+#   04/05/2024 Alex Medoff
+#   ------------------------
+#   Define the utility functions.
+#   ----------------------------------
+
+# import libraries
+import numpy as np
+import os
+import re
